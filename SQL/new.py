3c743836a6b83c91daa96339ab2bdaacc4547429
temii <taimoorrauf607@gmail.com>
2025-02-12 20:21:16 +0500

    delete extra file

--- SQL/new.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load the CSV file
-df = pd.read_csv('fifa.csv', encoding='utf-8', errors='replace')
-
-# Save the cleaned CSV back to file
-df.to_csv('fifa_cleaned.csv', index=False, encoding='utf-8')
